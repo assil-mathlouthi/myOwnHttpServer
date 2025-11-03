9b9110c655eb7bbf46b4bb5eeaeff96e53be67e3
assil <astradhdbjd@gmail.com>
2025-11-03 12:06:33 +0100

    read the data as lines

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-func main()  {
-	fmt.Printf("Our http server 1.0 is comming")
+func main() {
+	myFile, err := os.Open("message.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer myFile.Close()
+
+	buffer := make([]byte, 8)
+
+	line := ""
+
+	for {
+		n, err := myFile.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		data := string(buffer[:n])
+
+		if p := strings.Index(data, "\n"); p != -1 {
+			line += data[:p]
+			fmt.Printf("read:%s\n", line)
+			line = data[p+1:]
+		} else {
+			line += data
+		}
+	}
+	if len(line) != 0 {
+		fmt.Println(line)
+	}
+
 }
