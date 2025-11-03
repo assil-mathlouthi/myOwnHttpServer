a1e3e86e21c75bd974de14c4a161a385f72502f3
assil <astradhdbjd@gmail.com>
2025-11-03 20:30:12 +0100

    refactor: extract line reading logic into getLinesChannel function

--- main.go
+++ main.go
@@ -2,39 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	channel := make(chan string)
+
+	buffer := make([]byte, 8)
+	line := ""
+	go func() {
+		defer close(channel)
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				break
+			}
+			data := string(buffer[:n])
+
+			if p := strings.Index(data, "\n"); p != -1 {
+				line += data[:p]
+				channel <- line
+				line = data[p+1:]
+			} else {
+				line += data
+			}
+		}
+		if len(line) != 0 {
+			channel <- line
+		}
+	}()
+	return channel
+
+}
 
 func main() {
 	myFile, err := os.Open("message.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
+	getLinesChannel := getLinesChannel(myFile)
 	defer myFile.Close()
 
-	buffer := make([]byte, 8)
-
-	line := ""
-
-	for {
-		n, err := myFile.Read(buffer)
-		if err != nil {
-			break
-		}
-
-		data := string(buffer[:n])
-
-		if p := strings.Index(data, "\n"); p != -1 {
-			line += data[:p]
-			fmt.Printf("read:%s\n", line)
-			line = data[p+1:]
-		} else {
-			line += data
-		}
-	}
-	if len(line) != 0 {
-		fmt.Println(line)
+	for line := range getLinesChannel {
+		fmt.Printf("read:%s\n", line)
 	}
 
 }
